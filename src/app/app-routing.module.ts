ab78ffc3126f9658e7e7780dae7b27a3ed2ee539
Miruna Andriesei <andrieseimiruna10@gmail.com>
2022-04-22 23:18:02 +0300

    Loader page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'loading',
+    loadChildren: () => import('./pages/loading/loading.module').then( m => m.LoadingPageModule)
   }
 ];
 
