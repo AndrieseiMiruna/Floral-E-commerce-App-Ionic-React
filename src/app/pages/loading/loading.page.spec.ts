800188f9c65be0ab7272e626e1d28e518862f1e2
BeaJM <66080241+BeaJM@users.noreply.github.com>
2022-05-05 20:28:25 +0300

    update login and register

--- src/app/pages/loading/loading.page.spec.ts
+++ src/app/pages/loading/loading.page.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { LoadingPage } from './loading.page';
 
 describe('LoadingPage', () => {
   let component: LoadingPage;
   let fixture: ComponentFixture<LoadingPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoadingPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoadingPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +28,13 @@ describe('LoadingPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to login page after load', () =>{
+    spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    expect(router.navigate).toHaveBeenCalledWith('login');
+  })
+
 });
